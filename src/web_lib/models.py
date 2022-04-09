b675d6eae26d244582d2537d86080f9add6c9096
RidersWeb <samara_expert@mail.ru>
2022-04-09 12:21:28 +0300

    09.04.22

--- src/web_lib/models.py
+++ src/web_lib/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    class Meta:
+        verbose_name = 'Автор'
+        verbose_name_plural = 'Авторы'
+        ordering = ['name']
+
+    name = models.CharField(max_length=100)
+    age = models.PositiveIntegerField(verbose_name='Возраст автора')
+    tlt_type = models.CharField(max_length=1, verbose_name='тип литературы', choices=TYPES, default='a')
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    class Meta:
+        verbose_name = 'Книга'
+        verbose_name_plural = 'Книги'
+        ordering = ['name']
+
+    name = models.CharField(max_length=100)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    page_num = models.PositiveIntegerField()
+    published = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
